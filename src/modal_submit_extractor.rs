533973e926a782b9f38c648e4c4585b8b270d6f6
valkyrie_pilot <valk@randomairborne.dev>
2025-12-17 11:59:41 -0700

    Update modal extractor

--- src/modal_submit_extractor.rs
+++ src/modal_submit_extractor.rs
@@ -1,5 +1,7 @@
 use serde_json::Value;
-use twilight_model::application::interaction::{Interaction, InteractionData};
+use twilight_model::application::interaction::{
+    modal::ModalInteractionComponent, Interaction, InteractionData,
+};
 
 use crate::{handler::FromRequest, into_response_impls::BasicErrorReport, IntoResponse};
 
@@ -21,21 +23,54 @@ impl<S: Sync, T: for<'a> serde::Deserialize<'a>> FromRequest<S> for ModalSubmit<
             };
         };
 
-        let mut json_map = serde_json::Map::with_capacity(5);
+        let mut json_map = serde_json::Map::with_capacity(ms.components.len());
 
         for component in &ms.components {
-            for component in &component.components {
-                json_map.insert(
-                    component.custom_id.clone(),
-                    Value::String(component.value.clone().unwrap_or_else(String::new)),
-                );
-            }
+            map_component_extend(component, &mut json_map);
         }
 
         Ok(Self {
             custom_id: ms.custom_id.clone(),
             data: serde_json::from_value(Value::Object(json_map))?,
         })
+    }
+}
+
+macro_rules! jsonify_array {
+    ($map:expr, $val:expr) => {{
+        $map.insert(
+            $val.custom_id.clone(),
+            Value::Array(
+                $val.values
+                    .iter()
+                    .map(|v| Value::String(v.to_string()))
+                    .collect(),
+            ),
+        );
+    }};
+}
+
+fn map_component_extend(
+    component: &ModalInteractionComponent,
+    map: &mut serde_json::Map<String, Value>,
+) {
+    match component {
+        ModalInteractionComponent::Label(mil) => map_component_extend(&mil.component, map),
+        ModalInteractionComponent::ActionRow(ar) => {
+            for subcomponent in &ar.components {
+                map_component_extend(subcomponent, map);
+            }
+        }
+        ModalInteractionComponent::StringSelect(ss) => jsonify_array!(map, ss),
+        ModalInteractionComponent::UserSelect(us) => jsonify_array!(map, us),
+        ModalInteractionComponent::RoleSelect(rs) => jsonify_array!(map, rs),
+        ModalInteractionComponent::MentionableSelect(ms) => jsonify_array!(map, ms),
+        ModalInteractionComponent::TextInput(ti) => {
+            map.insert(ti.custom_id.clone(), Value::String(ti.value.clone()));
+        }
+        ModalInteractionComponent::ChannelSelect(cs) => jsonify_array!(map, cs),
+        ModalInteractionComponent::FileUpload(fu) => jsonify_array!(map, fu),
+        ModalInteractionComponent::TextDisplay(_) | ModalInteractionComponent::Unknown(_) => {}
     }
 }
 
